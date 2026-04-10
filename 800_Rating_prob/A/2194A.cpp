942408db0a2f9a1ebb787e85da5f2cb798c95112
avinashkumar159357 <aviofficial159357@gmail.com>
2026-04-10 20:51:19 +0530

    create 2194A Lawn mower

---
+++ 800_Rating_prob/A/2194A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long long n,m; cin>>n>>m;
+    long long req=n/m;
+    cout<<n-req<<"\n";
+}    
+
+
+int main() {
+    ios :: sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin>>t;
+    while(t--) {
+        solve();
+    }
+}
