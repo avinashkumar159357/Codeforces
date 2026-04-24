33f9c957a3a6540ecf350cab8300d643b5dd7cc7
avinashkumar159357 <aviofficial159357@gmail.com>
2026-04-24 21:30:22 +0530

    create 2065A skibidus and Amoh'u

---
+++ 800_RATED_PROBLEMS/2065A.cpp
@@ -0,0 +1,26 @@
+//File name:Skibidus and Amog'u
+//Author:Avinash kumar
+//Email Address: aviofficial159357@gmail.com
+//Problem ID: 2065A
+//Description:brute force ,constructive algorithm,implementation,strings greedy(*800)
+//Last Changed:24-4-2026
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    string s; cin>>s;
+    s.erase(s.size()-2);
+    s=s+'i';
+    cout<<s<<"\n";
+}
+int main() {
+    ios :: sync_with_stdio(0);
+    cin.tie(0);
+        
+    int t; cin>>t;
+    while(t--) {
+        solve();
+    }
+
+}
