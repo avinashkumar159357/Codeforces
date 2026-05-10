afe189598d9e8a7fe8315edc7d885a875267db24
avinashkumar159357 <aviofficial159357@gmail.com>
2026-05-10 20:01:48 +0530

    Pizza time

---
+++ 800_RATED_PROBLEMS/2156A.cpp
@@ -0,0 +1,32 @@
+//Problem name:Pizza time
+//Author:Avinash kumar (A_K_0_7)
+//Email Address: aviofficial159357@gmail.com
+//Problem ID:2156A
+//Description:constructive algorithm,greedy,math(*800)
+//Last Changed:5-05-2026
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long long n; cin>>n;
+    cout<<(n-1)/2<<"\n";  
+
+    //since m1<=m2<=m3 and m3 is nothing but amount of slices to be disributed 
+}   //at the next day to hao and alex , the interesting thing is that the 
+    //game is ending by alex eating atmost 2 slices ,so the optimal choice
+    //is we end the game by giving alex 1 slice so that hao would be able 
+    //to recieve more slices in previous days, now comes the most interesting part
+    //m1(hao)<=m2(alex),so the optimal choice for hao to have maximum is we give him
+    //no of slices equal to alex ,that is why we then divide total slices by 2
+    //after removing the ending slice which alex will get..
+int main() {
+    ios :: sync_with_stdio(0);
+    cin.tie(0);
+        
+    int t;  cin>>t;
+    while(t--) {
+        solve();
+    }
+
+}    
