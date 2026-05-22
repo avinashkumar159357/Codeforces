4a90bfc563d9524c7cc4167ef867792bb693e677
avinashkumar159357 <aviofficial159357@gmail.com>
2026-05-22 21:39:51 +0530

    Perfect root

---
+++ 800_RATED_PROBLEMS/2185A.cpp
@@ -0,0 +1,28 @@
+//Problem name:Perfect Root
+//Author:Avinash kumar (A_K_0_7)
+//Email Address: aviofficial159357@gmail.com
+//Problem ID:2185A
+//Description:constructive algorithm ,math (*800)
+//Last Changed:22-5-2026
+#include<bits/stdc++.h>
+using namespace std;    
+#define ll long long
+#define fastio ios :: sync_with_stdio(0);cin.tie(0);
+
+void solve(){
+    int n; cin>>n;
+
+    for (int i = 0; i < n; i++) cout<<i+1<<" ";
+    cout<<"\n";
+    
+}
+int main() {
+    fastio;
+    int TC; cin>>TC;
+    while(TC--) {
+        solve();
+    }
+
+}    
+
+    
