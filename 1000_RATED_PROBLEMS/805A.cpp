e58e2faf3ea1cc6693a6d01c302a0c3c69e58bc5
avinashkumar159357 <aviofficial159357@gmail.com>
2026-07-07 17:58:45 +0530

    Fake NP

---
+++ 1000_RATED_PROBLEMS/805A.cpp
@@ -0,0 +1,26 @@
+//File name:Fake NP
+//Author:Avinash kumar
+//Email Address: aviofficial159357@gmail.com
+//Problem ID:805A
+//Description:greedy, math(*1000)
+//Last Changed:07-07-2026
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){ 
+    ll l,r; cin>>l>>r;
+    if(l==r && l%2!=0) cout<<l;
+    else cout<<"2";
+}
+
+
+int main() {
+    ios :: sync_with_stdio(0);
+    cin.tie(0);
+    int t=1;// cin>>t;
+    while(t--) {
+        solve();
+    }
+
+}
